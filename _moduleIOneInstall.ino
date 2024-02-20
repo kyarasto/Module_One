6ff984156d6ccef32b6584f7c0c2ef7c6ad7f503
kyarasto <113846467+kyarasto@users.noreply.github.com>
2024-02-20 13:38:59 -0500

    Update _moduleIOneInstall.ino

--- _moduleIOneInstall.ino
+++ _moduleIOneInstall.ino
@@ -1,14 +1,14 @@
 #include <TFT_eSPI.h>
+//download as zip and add to libraries folder in Arduino folder https://github.com/pangodream/18650CL
 #include <Pangodream_18650_CL.h>
 #include <SPI.h>
-
-TFT_eSPI tft = TFT_eSPI();  // Invoke library, pins defined in User_Setup.h
 
 #define ADC_PIN 34
 #define CONV_FACTOR 1.8
 #define READS 20
 Pangodream_18650_CL BL(ADC_PIN, CONV_FACTOR, READS);
 
+TFT_eSPI tft = TFT_eSPI();  // Invoke library, pins defined in User_Setup.h
 #define BUTTON_PIN_BITMASK 0x800000000 // 2^35 in hex
 
 void IRAM_ATTR goToSleep() {
@@ -54,7 +54,7 @@ void loop() {
   delay(1000);
 }
 
-// From transparent sprite demo example
+// Used transparent sprite demo example 
 void drawStar(int x, int y, int star_color) {
   TFT_eSprite img = TFT_eSprite(&tft);
   
@@ -71,4 +71,7 @@ void drawStar(int x, int y, int star_color) {
   
   img.pushSprite(x, y, TFT_TRANSPARENT); // display sprite at random coordinates!
   img.deleteSprite(); // delete sprite to free memory
+
+// ESP32 TTGO T1 power demo by Mark Santolucito used
+
 }
